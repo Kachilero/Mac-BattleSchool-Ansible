ba522f522b78c93f25243724405f0a648623b8c7
Josh Fabean <josh@codekoalas.com>
2014-09-17 10:49:42 -0500

    changes

---
+++ README.md
@@ -0,0 +1,9 @@
+#Assumed...
+
+* already installed MAMP
+* SSH keys already set and accepted on servers
+* ~/.drush/myinfo.php is created
+should look like:
+`<?php
+$user = 'josh.fabean';
+$sites_dir = '/Users/joshfabean/web_development/sites';`
--- playbooks/dnsmasq.sh
+++ playbooks/dnsmasq.sh
@@ -11,3 +11,8 @@ composer install;
 cd /Applications/MAMP/conf/apache;
 cp ~/.battleschool/httpd.conf .;
 cp ~/.battleschool/extra-httpd-vhosts.conf ./extra/httpd-vhosts.conf;
+cd ~/.drush/;
+git clone git@aegir2.ryanwyse.com:/home/git/drush-aliases.git;
+cp drush-aliases/* .;
+mv drush-aliases/.git/ .;
+rm -Rf drush-aliases/;
