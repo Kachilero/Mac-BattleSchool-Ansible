9d81fa1c694b4393e571a941bbe643eade1284ab
Josh Fabean <josh@codekoalas.com>
2014-09-17 12:06:28 -0500

    we are getting crazy

--- playbooks/dnsmasq.sh
+++ playbooks/dnsmasq.sh
@@ -12,7 +12,7 @@ echo "Please enter your computer username (/Users/USERNAME/)";
 read UserName; echo "export PATH=$PATH:/Users/$UserName/drush" >> ~/.profile;
 cd /Applications/MAMP/conf/apache;
 cp ~/.battleschool/httpd.conf .;
-cp ~/.battleschool/extra-httpd-vhosts.conf ./extra/httpd-vhosts.conf;
+#cp ~/.battleschool/extra-httpd-vhosts.conf ./extra/httpd-vhosts.conf;
 mkdir ~/.drush/;
 cd ~/.drush/;
 git clone git@aegir2.ryanwyse.com:/home/git/drush-aliases.git;
@@ -22,3 +22,19 @@ rm -Rf drush-aliases/;
 echo "Please enter your server username (ssh USERNAME@server.com)";
 read ServerName; echo '<?php \n $user = '"'""$ServerName""'"'; \n $sites_dir = "/Users/'$UserName'/Sites";' > ~/.drush/myinfo.php;
 php auto-git.php;
+echo '<VirtualHost *:80>
+  VirtualDocumentRoot /Users/'"$UserName"'/Sites/%1
+  ServerAdmin webmaster@local.local
+  ServerName locals.dev
+  ServerAlias *.dev
+  ErrorLog "logs/dynamic-error.local"
+  CustomLog "logs/dynamic.local" common
+</VirtualHost>
+<VirtualHost *:80>
+    VirtualDocumentRoot /Users/'"$UserName"'/Sites/%1
+    ServerAdmin webmaster@local.local
+    ServerName xip.io
+    ServerAlias *.xip.io
+    ErrorLog "logs/xip-dynamic-error.local"
+    CustomLog "logs/xip-dynamic.local" common
+</VirtualHost>' >> /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf;
