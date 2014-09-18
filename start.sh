82444adf8c325c6aa56fad40fae937c7d8d1248c
Josh Fabean <joshfabean@gmail.com>
2014-09-17 22:29:12 -0500

    Update start.sh
    This should work now

--- start.sh
+++ start.sh
@@ -1,20 +1,24 @@
 #!/bin/bash/
 
 #install pip
+echo 'installing battleschool';
 sudo easy_install pip;
 sudo pip install battleschool;
 mkdir ~/.battleschool;
 
 # move everything to .battleschool directory
+echo 'moving everything to the battleschool directory';
 cp -R ~/Downloads/Mac-BattleSchool-Ansible-master/* ~/.battleschool/;
 cd ~/.battleschool;
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew doctor;
 
 # run battleschool
+echo 'running battleschool';
 battle --config-file ~/.battleschool/config.yml --module-path=/usr/share/battleschool/library --ask-sudo-pass;
 
 #config dnmasq
+echo 'config dnsmasq';
 cp $(brew list dnsmasq | grep /dnsmasq.conf.example$) /usr/local/etc/dnsmasq.conf;
 sudo cp $(brew list dnsmasq | grep /homebrew.mxcl.dnsmasq.plist$) /Library/LaunchDaemons/;
 echo 'address=/.dev/127.0.0.1' > /usr/local/etc/dnsmasq.conf;
@@ -22,6 +26,7 @@ sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist;
 echo 'nameserver 127.0.0.1' > /etc/resolver/dev;
 
 #download drush
+echo 'download drush';
 cd ~/Downloads; 
 wget https://github.com/drush-ops/drush/archive/master.zip;
 unzip master.zip;
@@ -38,34 +43,27 @@ if [ ! -f /usr/bin/drush ]; then
 fi
 
 #download/config mamp
+echo 'download mamp';
 if [ ! -f /Applications/MAMP/README.rtf ]; then
     #if we cannot find readme then mamp isn't installed
-    wget -P ~/Downloads/ http://downloads.mamp.info/MAMP-PRO/releases/3.0.6/MAMP_MAMP_PRO_3.0.6.pkg;
+    wget -P ~/Downloads/ http://downloads10.mamp.info/MAMP-PRO/releases/3.0.6/MAMP_MAMP_PRO_3.0.6.pkg;
     open ~/Downloads/MAMP_MAMP_PRO_3.0.6.pkg;
+    echo 'Hit enter when done installing MAMP';
+    read WaitingForEnter;
 fi
+echo 'configure mamp';
 cd /usr/local/bin; sudo ln -s /Applications/MAMP/Library/bin/mysql mysql;
 cd /Applications/MAMP/conf/apache;
 cp ~/.battleschool/httpd.conf .;
 #cp ~/.battleschool/extra-httpd-vhosts.conf ./extra/httpd-vhosts.conf;
-cp ../php.ini /Applications/MAMP/conf/php5.4.30/php.ini;
-cp ../php.ini /Applications/MAMP/bin/php/php5.4.30/conf/php.ini;
+cp ~/.battleschool/php.ini /Applications/MAMP/conf/php5.4.30/php.ini;
+cp ~/.battleschool/php.ini /Applications/MAMP/bin/php/php5.4.30/conf/php.ini;
 rm -R /Applications/MAMP/bin/php/php5.1.6;
 rm -R /Applications/MAMP/bin/php/php5.2.17;
 rm -R /Applications/MAMP/bin/php/php5.3.28;
 rm /Applications/MAMP/db/mysql/ib_logfile0;
 rm /Applications/MAMP/db/mysql/ib_logfile1;
-cp ../my.cnf /Applications/MAMP/conf/my.cnf;
-
-#set up autogit drush
-mkdir ~/.drush/;
-cd ~/.drush/;
-git clone git@aegir2.ryanwyse.com:/home/git/drush-aliases.git;
-cp drush-aliases/* .;
-mv drush-aliases/.git/ .;
-rm -Rf drush-aliases/;
-echo "Please enter your server username (ssh USERNAME@server.com)";
-read ServerName; echo '<?php \n $user = '"'""$ServerName""'"'; \n $sites_dir = "/Users/'$UserName'/Sites";' > ~/.drush/myinfo.php;
-
+cp ~/.battleschool/my.cnf /Applications/MAMP/conf/my.cnf;
 #build virtual hosts
 echo '<VirtualHost *:80>
   VirtualDocumentRoot /Users/'"$UserName"'/Sites/%1
@@ -83,4 +81,23 @@ echo '<VirtualHost *:80>
     ErrorLog "logs/xip-dynamic-error.local"
     CustomLog "logs/xip-dynamic.local" common
 </VirtualHost>' >> /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf;
-#php auto-git.php;
+
+#set up autogit drush
+echo 'setting up the drush auto-git';
+mkdir ~/.drush/;
+cd ~/.drush/;
+git clone git@aegir2.ryanwyse.com:/home/git/drush-aliases.git;
+cp drush-aliases/* .;
+mv drush-aliases/.git/ .;
+rm -Rf drush-aliases/;
+echo "Please enter your server username (ssh USERNAME@server.com)";
+read ServerName; echo '<?php \n $user = '"'""$ServerName""'"'; \n $sites_dir = "/Users/'$UserName'/Sites";' > ~/.drush/myinfo.php;
+
+echo 'ALMOST DONE! Going to open MAMP for the first time!';
+echo 'Choose just regular MAMP, not MAMP PRO';
+open -a 'MAMP';
+echo 'Once servers are running hit enter';
+read WaintingOnEnter;
+
+echo 'about to pull in all the drush auto-git sites (this may take awhile)';
+php auto-git.php;
