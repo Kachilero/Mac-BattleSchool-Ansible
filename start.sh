dffb978d898040f76f157f7b813bf1d5bffaed2e
Josh Fabean <joshfabean@gmail.com>
2014-09-17 22:36:22 -0500

    Update start.sh
    now it won't keep appending the virtual host each time you run this script

--- start.sh
+++ start.sh
@@ -55,32 +55,35 @@ echo 'configure mamp';
 cd /usr/local/bin; sudo ln -s /Applications/MAMP/Library/bin/mysql mysql;
 cd /Applications/MAMP/conf/apache;
 cp ~/.battleschool/httpd.conf .;
-#cp ~/.battleschool/extra-httpd-vhosts.conf ./extra/httpd-vhosts.conf;
 cp ~/.battleschool/php.ini /Applications/MAMP/conf/php5.4.30/php.ini;
 cp ~/.battleschool/php.ini /Applications/MAMP/bin/php/php5.4.30/conf/php.ini;
+cp ~/.battleschool/my.cnf /Applications/MAMP/conf/my.cnf;
 rm -R /Applications/MAMP/bin/php/php5.1.6;
 rm -R /Applications/MAMP/bin/php/php5.2.17;
 rm -R /Applications/MAMP/bin/php/php5.3.28;
 rm /Applications/MAMP/db/mysql/ib_logfile0;
 rm /Applications/MAMP/db/mysql/ib_logfile1;
-cp ~/.battleschool/my.cnf /Applications/MAMP/conf/my.cnf;
 #build virtual hosts
-echo '<VirtualHost *:80>
-  VirtualDocumentRoot /Users/'"$UserName"'/Sites/%1
-  ServerAdmin webmaster@local.local
-  ServerName locals.dev
-  ServerAlias *.dev
-  ErrorLog "logs/dynamic-error.local"
-  CustomLog "logs/dynamic.local" common
-</VirtualHost>
-<VirtualHost *:80>
-    VirtualDocumentRoot /Users/'"$UserName"'/Sites/%1
-    ServerAdmin webmaster@local.local
-    ServerName xip.io
-    ServerAlias *.xip.io
-    ErrorLog "logs/xip-dynamic-error.local"
-    CustomLog "logs/xip-dynamic.local" common
-</VirtualHost>' >> /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf;
+if [ ! -f /Applications/MAMP/conf/apache/extra/ran-script.txt ]; then
+    #we don't want to keep appending this onto the end, that probably does bad things
+    echo '<VirtualHost *:80>
+      VirtualDocumentRoot /Users/'"$UserName"'/Sites/%1
+      ServerAdmin webmaster@local.local
+      ServerName locals.dev
+      ServerAlias *.dev
+      ErrorLog "logs/dynamic-error.local"
+      CustomLog "logs/dynamic.local" common
+    </VirtualHost>
+    <VirtualHost *:80>
+        VirtualDocumentRoot /Users/'"$UserName"'/Sites/%1
+        ServerAdmin webmaster@local.local
+        ServerName xip.io
+        ServerAlias *.xip.io
+        ErrorLog "logs/xip-dynamic-error.local"
+        CustomLog "logs/xip-dynamic.local" common
+    </VirtualHost>' >> /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf;
+    touch /Applications/MAMP/conf/apache/extra/ran-script.txt;
+fi
 
 #set up autogit drush
 echo 'setting up the drush auto-git';
